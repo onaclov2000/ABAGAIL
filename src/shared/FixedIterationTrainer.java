f3f5cd23bf6b4237ae2f46c81c3c9432bcb1242a
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-20 20:37:57 -0600

    Update FixedIterationTrainer.java

--- src/shared/FixedIterationTrainer.java
+++ src/shared/FixedIterationTrainer.java
@@ -1,5 +1,6 @@
 package shared;
-
+import opt.EvaluationFunction;
+import opt.OptimizationAlgorithm;
 /**
  * A fixed iteration trainer
  * @author Andrew Guillory gtg008g@mail.gatech.edu
@@ -17,14 +18,17 @@ public class FixedIterationTrainer implements Trainer {
      */
     private int iterations;
     
+    private EvaluationFunction eval;
+    
     /**
      * Make a new fixed iterations trainer
      * @param t the trainer
      * @param iter the number of iterations
      */
-    public FixedIterationTrainer(Trainer t, int iter) {
+    public FixedIterationTrainer(Trainer t, int iter, EvaluationFunction ef) {
         trainer = t;
         iterations = iter;
+        eval = ef;
     }
 
     /**
@@ -36,6 +40,29 @@ public class FixedIterationTrainer implements Trainer {
             sum += trainer.train();
         }
         return sum / iterations;
+    }
+     public int train(int opt) {
+        double sum = 0;
+        int i = 0;
+        int current_val = 0;
+        double previous_opt = 0.0;
+        OptimizationAlgorithm temp = (OptimizationAlgorithm) trainer;
+        while ((int)eval.value(temp.getOptimal()) < opt && current_val < 2000){
+                i++;
+                if (eval.value(temp.getOptimal()) == previous_opt){
+                    current_val++;
+                }
+                else{
+                    current_val = 0;    
+                }
+                previous_opt = eval.value(temp.getOptimal());
+                sum += trainer.train();
+                temp = (OptimizationAlgorithm) trainer;
+        }
+        if (current_val == 2000){
+            return i - current_val;
+        }
+        return i;
     }
     
 
